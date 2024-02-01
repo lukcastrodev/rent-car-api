dd493f8a33b4148a37bf422f145149f1a5b10faf
lukcastrodev <lucasrockcastro@gmail.com>
2024-02-01 15:01:56 -0300

    add password util

---
+++ src/main/java/com/ctech/rentcarapi/utils/PasswordEncoder.java
@@ -0,0 +1,10 @@
+package com.ctech.rentcarapi.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+    public static String encoder(String password){
+        BCryptPasswordEncoder hash = new BCryptPasswordEncoder();
+        return hash.encode(password);
+    }
+}
