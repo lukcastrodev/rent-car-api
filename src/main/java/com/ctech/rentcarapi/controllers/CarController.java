2e4a1b7f05ccea6704b4613969f340d53ea476fb
lukcastrodev <lucasrockcastro@gmail.com>
2024-02-01 19:11:19 -0300

    remove rent reposability from car controller

--- src/main/java/com/ctech/rentcarapi/controllers/CarController.java
+++ src/main/java/com/ctech/rentcarapi/controllers/CarController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ctech.rentcarapi.dtos.CarDTO;
-import com.ctech.rentcarapi.dtos.UserDTO;
 import com.ctech.rentcarapi.services.CarService;
-import com.ctech.rentcarapi.services.RentCarService;
 
 import jakarta.validation.constraints.NotNull;
 
@@ -19,20 +17,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
 @RestController
 @RequestMapping("/api/car")
 public class CarController {
     
     private final CarService carService;
-    private final RentCarService rentCarService;
 
-    public CarController(CarService carService,
-            RentCarService rentCarService
-    ){
+    public CarController(CarService carService){
         this.carService = carService;
-        this.rentCarService = rentCarService;
     }
 
     @GetMapping
@@ -48,11 +40,6 @@ public class CarController {
     @PostMapping
     public CarDTO create(@RequestBody @NotNull CarDTO car) {
         return this.carService.create(car);
-    }
-
-    @GetMapping("/rented-car/{id}")
-    public List<UserDTO> listUsersByCarId(@PathVariable @NotNull Long id) {
-        return this.rentCarService.findUsersByCarId(id);
     }
 
     @PutMapping("/{id}")
