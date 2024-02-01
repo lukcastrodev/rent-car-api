8753ec5d122287d8d063f3d8fbb22816d19ecf5e
lukcastrodev <lucasrockcastro@gmail.com>
2024-02-01 19:14:21 -0300

    set util constructor to private

--- src/main/java/com/ctech/rentcarapi/utils/PasswordEncoder.java
+++ src/main/java/com/ctech/rentcarapi/utils/PasswordEncoder.java
@@ -3,6 +3,11 @@ package com.ctech.rentcarapi.utils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoder {
+
+    private PasswordEncoder(){
+
+    }
+    
     public static String encoder(String password){
         BCryptPasswordEncoder hash = new BCryptPasswordEncoder();
         return hash.encode(password);
