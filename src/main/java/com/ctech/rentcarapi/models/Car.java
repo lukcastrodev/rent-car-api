57cfe0880303bd4405b3fed24e8efcf4038f0065
lukcastrodev <lucasrockcastro@gmail.com>
2024-02-01 15:02:50 -0300

    add car entity

---
+++ src/main/java/com/ctech/rentcarapi/models/Car.java
@@ -0,0 +1,103 @@
+package com.ctech.rentcarapi.models;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Length(min = 3, max = 100)
+    private String brand;
+    
+    @NotNull
+    @Length(min = 3, max = 100)
+    private String model;
+    
+    @NotNull
+    @Length(max = 4)
+    private String year;
+
+    @NotNull
+    private BigDecimal fuel;
+
+    @NotNull
+    @Length(max = 10)
+    private String condition;
+
+    @ManyToMany(mappedBy = "cars", cascade = CascadeType.ALL)
+    private List<User> users = new ArrayList<>();
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getYear() {
+        return this.year;
+    }
+
+    public void setFuel(BigDecimal fuel) {
+        this.fuel = fuel;
+    }
+
+    public BigDecimal getFuel() {
+        return this.fuel;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getCondition() {
+        return this.condition;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return this.users;
+    }
+}
